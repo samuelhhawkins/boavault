07655ebb843ce8b7f2b7d6a55a0dc28fb4375093
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-06-12 09:22:52 -0700

    updated settings env

---
+++ boavault/.bash_profile
@@ -0,0 +1,6 @@
+export DB_USER="samuel.h.hawkins1@gmail.com"
+export DB_PASSWORD="Gumball0"
+
+#  DJANGO STORE variables
+export SEC_KEY = '4b21bd21486cd5188d447d4dabf25d517f888e1414e935f1'
+export DEBUG_VALUE='True'
--- boavault/boavault/settings.py
+++ boavault/boavault/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h+87)%80xkf-+#1-27n%my4)mbqezd*r__)#(=^^6yj53z7wp7'
-
+# SECRET_KEY = 'h+87)%80xkf-+#1-27n%my4)mbqezd*r__)#(=^^6yj53z7wp7'
+SECRET_KEY = os.environ.get('SEC_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boavault.herokuapp.com'']
 
 
 # Application definition
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('samuel.h.hawkins1@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('Gumball0')
+EMAIL_HOST_USER = os.environ.get('DB_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_PASSWORD')
