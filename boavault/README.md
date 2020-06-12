ca6e0e94c602fa68cda07fbf7022c795e70d9d77
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-06-12 09:56:55 -0700

    new heroku app

--- boavault/README.md
+++
@@ -1,2 +0,0 @@
-# project4
-boavault
--- proj4/boavault/settings.py
+++ proj4/boavault/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'h+87)%80xkf-+#1-27n%my4)mbqezd*r__)#(=^^6yj53z7wp7'
-SECRET_KEY = os.environ.get('SEC_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
