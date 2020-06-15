f301c3f89dda76d91e017d9eb7d1894e7235b08b
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-06-15 12:01:43 -0700

    created

--- proj4/boavault/settings.py
+++ proj4/boavault/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'boavault.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': os.path.join(BASE_DIR, 'db.postgres'),
     }
 }
 
